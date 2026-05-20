350c725ea2e4b2311ec4cbddc1dcd42e75d04c1f
rajveer-0402 <2102508787@svyasa-sas.edu.in>
2026-05-20 13:43:08 +0530

    did some changes

--- couplewatch/src/pages/Home.jsx
+++ couplewatch/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Navbar from "../components/Navbar";
 import { FloatingHearts } from "../components/FloatingHearts";
 
 const IconHeart = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="currentColor" className="text-white"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/></svg>
 );
 
 const IconSync = () => (
