3293474191e325236d57f34d4da8014522d7d8f1
rajveer-0402 <2102508787@svyasa-sas.edu.in>
2026-05-23 11:02:04 +0530

    fix: exhaustive STUN server list for better NAT traversal

--- couplewatch/src/hooks/useWebRTC.js
+++ couplewatch/src/hooks/useWebRTC.js
@@ -132,7 +132,16 @@ export function useWebRTC(user, channelRef) {
         { urls: "stun:stun.l.google.com:19302" },
         { urls: "stun:stun1.l.google.com:19302" },
         { urls: "stun:stun2.l.google.com:19302" },
-        { urls: "stun:stun.services.mozilla.com" },
+        { urls: "stun:stun3.l.google.com:19302" },
+        { urls: "stun:stun4.l.google.com:19302" },
+        { urls: "stun:stun.ekiga.net" },
+        { urls: "stun:stun.ideasip.com" },
+        { urls: "stun:stun.schlund.de" },
+        { urls: "stun:stun.stunprotocol.org:3478" },
+        { urls: "stun:stun.voiparound.com" },
+        { urls: "stun:stun.voipbuster.com" },
+        { urls: "stun:stun.voipstunt.com" },
+        { urls: "stun:stun.voxgratia.org" },
         // Add OpenRelay Project Free TURN servers
         {
           urls: "turn:openrelay.metered.ca:443",
@@ -145,6 +154,7 @@ export function useWebRTC(user, channelRef) {
           credential: "openrelayproject",
         }
       ],
+      iceTransportPolicy: "all",
       bundlePolicy: "balanced"
     });
 
